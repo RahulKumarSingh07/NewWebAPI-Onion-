619fe1b162162c0c4e1e643d64a5df687f22c4ff
Rahul Kumar Singh <rahul1999ksingh@gmail.com>
2022-12-26 10:42:08 +0530

    Add files via upload

---
+++ DependencyInjectionServices.cs
@@ -0,0 +1,21 @@
+﻿using ReposatioryLayer.DataBaseEntity;
+using ReposatioryLayer;
+using ReposatioryLayer.ReposatioryServices;
+using ServicesLayer.Interfaces;
+using ServicesLayer;
+
+namespace WebApiArchitechture.AllDependencyServices
+{
+    public class DependencyInjectionServices
+    {
+        public static void AllDependencyServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+;            builder.Services.AddScoped(typeof(IAsynReposatiory<>), typeof(Reposatiory<>));
+            builder.Services.AddScoped(typeof(IReposatiory<>), typeof(Reposatiory<>));
+            builder.Services.AddScoped<IStudent, StudentService>();
+
+        }
+    }
+}
